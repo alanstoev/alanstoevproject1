e02ea162200ff82f27cda32d7414ca43190b8f4f
Alan Stoev <as4246@pleasantonusd.net>
2017-11-29 09:40:45 -0800

    added files

---
+++ project 1/HelloWorld.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ * @author (Alan Stoev) 
+ * @version (8/17/2017)
+ */
+public class HelloWorld
+{
+    public static void main()
+    {
+        System.out.println("Hello World!");
+    }    
+}
